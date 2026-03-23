70ffe479b000ea5478e4f96fb607423c6ba29d04
TobiaszChatys <TobiaszChatys1@gmail.com>
2026-03-23 11:29:02 +0000

    uploaded the experimental data and plotted it agaisnt the CFD

--- Main.m
+++ Main.m
@@ -3,19 +3,25 @@ clear; clc; close all;
 
 %% loading
 
-data02 = readmatrix('cp_y02case01_upper.csv');
-data09 = readmatrix('cp_y09case01_upper.csv');
+CFD02 = readmatrix('cp_y02case01_upper.csv');
+Experimental02 = readmatrix('cp_y02experimental_upper.csv');
 
-Cp = data02(:, 2);
-x = data02(:, 1);
+x_CFD = CFD02(:, 1);
+x_Experimental = Experimental02(:, 1);
 
-x_leading_edge = min(x);
-x_trailing_edge = max(x);
+Cp_CFD = CFD02(:, 2);
+Cp_Experimental = -Experimental02(:,2);
 
-c = x_trailing_edge - x_leading_edge;
+x_leading_edge = min(x_CFD);
+x_trailing_edge = max(x_CFD);
+
+c_CFD = x_trailing_edge - x_leading_edge;
 
 %% Non-Dimensionalise
 
-xi = (x - x_leading_edge)/ c;
+xi = (x_CFD - x_leading_edge)/ c_CFD;
 
-plot(xi, Cp);
+figure,
+plot(xi, Cp_CFD, 'Marker', 'o');
+hold on;
+plot(x_Experimental, Cp_Experimental,'Marker', 's');
