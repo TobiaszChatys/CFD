85c9b9d354380795821bf5b9fc6587219a066634
TobiaszChatys <TobiaszChatys1@gmail.com>
2026-03-23 00:36:14 +0000

    calculated the chord length, and non-dimensionalised the x

--- Main.m
+++ Main.m
@@ -6,3 +6,14 @@ clear; clc; close all;
 data02 = readmatrix('cp_y02case01_upper.csv');
 data09 = readmatrix('cp_y09case01_upper.csv');
 
+Cp = data02(:, 1);
+x = data02(:, 2);
+
+x_leading_edge = min(x);
+x_trailing_edge = max(x);
+
+c = x_trailing_edge - x_leading_edge;
+
+%% Non-Dimensionalise
+
+xi = (x - x_leading_edge)/ c;
