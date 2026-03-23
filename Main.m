2f3bfdd31b9a123bcefc80d5b93954c8ccc6441b
TobiaszChatys <TobiaszChatys1@gmail.com>
2026-03-23 11:52:05 +0000

    updated plotting to be more presentable

--- Main.m
+++ Main.m
@@ -6,22 +6,29 @@ clear; clc; close all;
 CFD02 = readmatrix('cp_y02case01_upper.csv');
 Experimental02 = readmatrix('cp_y02experimental_upper.csv');
 
-x_CFD = CFD02(:, 1);
-x_Experimental = Experimental02(:, 1);
+x_CFD02 = CFD02(:, 1);
+x_Experimental02 = Experimental02(:, 1);
 
-Cp_CFD = CFD02(:, 2);
-Cp_Experimental = -Experimental02(:,2);
+Cp_CFD02 = CFD02(:, 2);
+Cp_Experimental02 = -Experimental02(:,2);
 
-x_leading_edge = min(x_CFD);
-x_trailing_edge = max(x_CFD);
+x_leading_edge = min(x_CFD02);
+x_trailing_edge = max(x_CFD02);
 
-c_CFD = x_trailing_edge - x_leading_edge;
+c_CFD02 = x_trailing_edge - x_leading_edge;
 
 %% Non-Dimensionalise
 
-xi = (x_CFD - x_leading_edge)/ c_CFD;
+xi = (x_CFD02 - x_leading_edge)/ c_CFD02;
 
 figure,
-plot(xi, Cp_CFD, 'Marker', 'o');
+plot(xi, Cp_CFD02, 'Marker', 'o', 'DisplayName', 'CFD', ...
+  'MarkerSize', 10, 'LineStyle', 'none', 'LineWidth', 2);
 hold on;
-plot(x_Experimental, Cp_Experimental,'Marker', 's');
+plot(x_Experimental02, Cp_Experimental02,'Marker', 's', 'DisplayName', ...
+  'Experimental', 'MarkerSize', 10, 'LineStyle', 'none', 'LineWidth', 2);
+xlabel('$x/c$', 'FontSize', 20, 'Interpreter', 'latex');
+ylabel('$C_p$', 'FontSize', 20, 'Interpreter', 'latex');
+grid on;
+set(gca, 'FontSize', 20, 'TickLabelInterpreter', 'latex', 'linewidth', 1)
+legend('Location', 'best', 'FontSize', 20, 'Interpreter', 'latex');
