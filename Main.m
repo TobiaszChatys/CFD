35bb3555b4883d1ae868f612b1db95b5b70e84e9
TobiaszChatys <TobiaszChatys1@gmail.com>
2026-03-23 00:42:38 +0000

    fixed the data extraction and plotted the pressure coefficent aginst xi

--- Main.m
+++ Main.m
@@ -6,8 +6,8 @@ clear; clc; close all;
 data02 = readmatrix('cp_y02case01_upper.csv');
 data09 = readmatrix('cp_y09case01_upper.csv');
 
-Cp = data02(:, 1);
-x = data02(:, 2);
+Cp = data02(:, 2);
+x = data02(:, 1);
 
 x_leading_edge = min(x);
 x_trailing_edge = max(x);
@@ -17,3 +17,5 @@ c = x_trailing_edge - x_leading_edge;
 %% Non-Dimensionalise
 
 xi = (x - x_leading_edge)/ c;
+
+plot(xi, Cp);
